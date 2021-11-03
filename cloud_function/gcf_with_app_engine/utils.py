b94aac6ed321c619b233ff5831fdadf9554610c6
ptheru <47907051+ptheru@users.noreply.github.com>
2021-11-03 13:49:16 -0500

    Update utils.py

--- cloud_function/gcf_with_app_engine/utils.py
+++ cloud_function/gcf_with_app_engine/utils.py
@@ -8,54 +8,7 @@ SEQ_LENGTH = 25
 EMBED_DIM = 512
 FF_DIM = 512
 MODEL_PATH = '/tmp/checkpoint.ckpt'
-#MODEL_PATH = '/Users/ypxt035/Desktop/Priya/ml_deployment_tutorial/Image-captioning-exp/caption_model/checkpoint.ckpt'
 
-
-# def predict_json(project, region, model, instances, version=None):
-#     """Send json data to a deployed model for prediction.
-
-#     Args:
-#         project (str): project where the Cloud ML Engine Model is deployed.
-#         model (str): model name.
-#         instances ([Mapping[str: Any]]): Keys should be the names of Tensors
-#             your deployed model expects as inputs. Values should be datatypes
-#             convertible to Tensors, or (potentially nested) lists of datatypes
-#             convertible to Tensors.
-#         version (str): version of the model to target.
-#     Returns:
-#         Mapping[str: any]: dictionary of prediction results defined by the 
-#             model.
-#     """
-#     # Create the ML Engine service object
-#     prefix = "{}-ml".format(region) if region else "ml"
-#     api_endpoint = "https://{}.googleapis.com".format(prefix)
-#     client_options = ClientOptions(api_endpoint=api_endpoint)
-
-#     # Setup model path
-#     model_path = "projects/{}/models/{}".format(project, model)
-#     if version is not None:
-#         model_path += "/versions/{}".format(version)
-
-#     # Create ML engine resource endpoint and input data
-#     ml_resource = googleapiclient.discovery.build(
-#         "ml", "v1", cache_discovery=False, client_options=client_options).projects()
-#     instances_list = instances.numpy().tolist() # turn input into list (ML Engine wants JSON)
-    
-#     input_data_json = {"signature_name": "serving_default",
-#                        "instances": instances_list} 
-
-#     request = ml_resource.predict(name=model_path, body=input_data_json)
-#     response = request.execute()
-    
-#     # # ALT: Create model api
-#     # model_api = api_endpoint + model_path + ":predict"
-#     # headers = {"Authorization": "Bearer " + token}
-#     # response = requests.post(model_api, json=input_data_json, headers=headers)
-
-#     if "error" in response:
-#         raise RuntimeError(response["error"])
-
-#     return response["predictions"]
 
 def load_model():
     cnn_model = get_cnn_model()
@@ -93,15 +46,3 @@ def output_captions(tokenizer,dta):
 
 def index_to_word(tokenizer):
   return dict(zip(tokenizer.word_index.values(),tokenizer.word_index.keys()))
-
-
-def update_logger(image, user_caption=None):
-    """
-    Function for tracking feedback given in app, updates and returns 
-    logger dictionary.
-    """
-    logger = {
-        "image": image,
-        "user_caption": user_caption
-    }   
-    return logger
